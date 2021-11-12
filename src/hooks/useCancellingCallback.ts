ff83cb8c467ac89769ee5915f125e77eaa1b602a
Taran van Groenigen <tvangroenigen@outlook.com>
2021-11-12 19:39:35 +0100

    Fix small mistake

--- src/hooks/useCancellingCallback.ts
+++ src/hooks/useCancellingCallback.ts
@@ -1,11 +1,9 @@
 import CancellationToken from 'cancellationtoken'
-import { DependencyList, useCallback, useEffect, useMemo } from 'react'
+import { DependencyList, useEffect, useMemo } from 'react'
 
 const useCancellingCallback = <T extends (cancellationToken: CancellationToken, ...args: any[]) => any>(callback: T, deps: DependencyList) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const { token, cancel } = useMemo(() => CancellationToken.create(), deps)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const cb = useCallback(callback, deps)
 
   useEffect(() => {
     return () => {
@@ -14,6 +12,9 @@ const useCancellingCallback = <T extends (cancellationToken: CancellationToken, 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps)
 
-  return cb.bind(cb, token)
+  return useMemo(() => (
+    callback.bind(callback, token)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  ), deps)
 }
 export default useCancellingCallback
